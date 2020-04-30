a608a45f30640d8173f1c14850879889ab1babb6
Dalton Scharff <dalton.scharff@gmail.com>
2020-04-29 19:36:08 -0500

    Updates

---
+++ precache-manifest.a253463e019aefcc0b0cf949e64ae5c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8b56269071cebdddf90c342c71e672",
+    "url": "/multiplayer-spelling-bee/index.html"
+  },
+  {
+    "revision": "b744d6ca8adaf984b04f",
+    "url": "/multiplayer-spelling-bee/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d4bfde31c1a707afbd15",
+    "url": "/multiplayer-spelling-bee/static/js/2.2e9a9f33.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/multiplayer-spelling-bee/static/js/2.2e9a9f33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b744d6ca8adaf984b04f",
+    "url": "/multiplayer-spelling-bee/static/js/main.6d03144b.chunk.js"
+  },
+  {
+    "revision": "57ac630ef986212f5579",
+    "url": "/multiplayer-spelling-bee/static/js/runtime-main.be9a450a.js"
+  }
+]);
